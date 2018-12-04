ccf7275bf7791146f13f2df0f30a313b2ca15717
Ana Belisa <ana.belisa9403>
2018-12-04 00:41:11 -0500

    Add origin and destination as props to Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { compose, withProps, lifecycle } from "recompose";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer } from "react-google-maps";
+import { withGoogleMap, GoogleMap, DirectionsRenderer } from "react-google-maps";
 
 const Map = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC8brpUwCDdc8bMjBnGqgZvuW0nobn2Jho&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `600px` }} />,
+    mapElement: <div style={{ height: `80%`, width: `100%` }} />,
   }),
-  withScriptjs,
   withGoogleMap,
   lifecycle({
     componentDidMount() {
@@ -17,8 +15,8 @@ const Map = compose(
       const DistanceService = new window.google.maps.DistanceMatrixService();
 
       DirectionsService.route({
-        origin: new window.google.maps.LatLng(41.8507300, -87.6512600),
-        destination: new window.google.maps.LatLng(41.8525800, -87.6514100),
+        origin: new window.google.maps.LatLng(this.props.origin.lat, this.props.origin.lng),
+        destination: new window.google.maps.LatLng(this.props.destination.lat, this.props.destination.lng),
         travelMode: window.google.maps.TravelMode.DRIVING,
       }, (result, status) => {
         if (status === window.google.maps.DirectionsStatus.OK) {
@@ -31,28 +29,36 @@ const Map = compose(
       });
 
       DistanceService.getDistanceMatrix({
-        origins: [new window.google.maps.LatLng(41.8507300, -87.6512600)],
-        destinations: [new window.google.maps.LatLng(41.8525800, -87.6514100)],
+        origins: [new window.google.maps.LatLng(this.props.origin.lat, this.props.origin.lng)],
+        destinations: [new window.google.maps.LatLng(this.props.destination.lat, this.props.destination.lng)],
         travelMode: window.google.maps.TravelMode.DRIVING,
         avoidHighways: false,
         avoidTolls: false,
         unitSystem: window.google.maps.UnitSystem.IMPERIAL
       },
         (result, status) => {
-          console.log(result);
-          console.log(status);
+          this.setState({
+            originAddresses: result.originAddresses,
+            destinationAddresses: result.destinationAddresses,
+            distance: result.rows[0].elements[0].distance.text,
+            duration: result.rows[0].elements[0].duration.text,
+          })
         }
       );
     }
   })
 )((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 41.8507300, lng: -87.6512600 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 41.8507300, lng: -87.6512600 }} />}
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
-  </GoogleMap>
+  <div>
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: 4.624335, lng: -74.063644 }}
+    >
+      {props.directions && <DirectionsRenderer directions={props.directions} />}
+    </GoogleMap>
+    <div className="sub-information">
+      Tiempo promedio de viaje es <strong>{props.duration}</strong> con <strong>{props.distance}</strong> entre <strong>{props.originValue} ({props.originAddresses})</strong> y <strong>{props.destinationValue} ({props.destinationAddresses})</strong>
+    </div>
+  </div>
 )
 
 export default Map;
